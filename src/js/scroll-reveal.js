e178dfb0ada2b8f7e0eab20b77004207c3625f09
yaboi <tyler@tylergoelz.com>
2019-02-09 19:03:48 -0800

    ADD - scroll reveal to add a bit of motion to the site.

---
+++ src/js/scroll-reveal.js
@@ -0,0 +1,26 @@
+import ScrollReveal from 'scrollreveal';
+
+const commonOptions = {
+	distance: '20px',
+	easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+	delay: 150,
+}
+
+ScrollReveal().reveal('.reveal-bottom', { 
+	...commonOptions,
+	origin: 'bottom',
+	distance: '40px',
+	duration: 800,
+});
+
+ScrollReveal().reveal('.reveal-left', {
+	...commonOptions,
+	origin: 'left',
+	duration: 500,
+});
+
+ScrollReveal().reveal('.reveal-right', { 
+	...commonOptions,
+	origin: 'right',
+  duration: 500,
+});
